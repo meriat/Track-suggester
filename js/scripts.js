4fef2bb60a7dc34e53cd966b66b0ff712e470d8f
Meria Thomas <josemeria93@gmail.com>
2018-08-10 13:07:19 -0700

    add form to enter name

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,62 @@
 $(document).ready(function(){
-  $("form#test").submit(function(event){
-    event.preventDefault();
 
-    var answer1 = $("input:radio[name=q1]:checked").val();
-    var answer2 = $("input:radio[name=q2]:checked").val();
-    var answer3 = $("input:radio[name=q3]:checked").val();
-    var answer4 = $("input:radio[name=q4]:checked").val();
-    var answer5 = $("input:radio[name=q5]:checked").val();
+$("form#input").submit(function(event){
+  event.preventDefault();
+    $("form#test").fadeIn();
 
-     var array = [answer1, answer2, answer3, answer4, answer5];
+    $("form#test").submit(function(event){
+      event.preventDefault();
 
-    var ruby = 0;
-    var css = 0;
-    var csharp = 0;
+      var answer1 = $("input:radio[name=q1]:checked").val();
+      var answer2 = $("input:radio[name=q2]:checked").val();
+      var answer3 = $("input:radio[name=q3]:checked").val();
+      var answer4 = $("input:radio[name=q4]:checked").val();
+      var answer5 = $("input:radio[name=q5]:checked").val();
 
-    for(var i=0; i<5; i++)
-    {
-      answer = array[i];
+       var array = [answer1, answer2, answer3, answer4, answer5];
 
-      if(answer === "a"){
-        csharp ++;
+      var ruby = 0;
+      var css = 0;
+      var csharp = 0;
+
+      for(var i=0; i<5; i++)
+      {
+        answer = array[i];
+
+        if(answer === "a"){
+          csharp ++;
+        }
+         else if(answer === "b"){
+          ruby ++;
+        }
+        else{
+          css ++;
+        }
+
       }
-       else if(answer === "b"){
-        ruby ++;
+
+      var max = Math.max(csharp,ruby,css);
+
+      if (max === ruby) {
+        $("#result").show();
+        $("span.name").text(name);
+        $("span.path").text("Ruby/Rails");
       }
-      else{
-        css ++;
+      else if (max === css) {
+        $("#result").show();
+        $("span.name").text(name);
+        $("span.path").text("CSS/React");
+      }
+      else {
+        $("#result").show();
+        $("span.name").text(name);
+        $("span.path").text("C#/.NET");
       }
 
-    }
+    });
+});
 
-    var max = Math.max(csharp,ruby,css);
 
-    if (max === ruby) {
-      $("#result").show();
-      $("span.path").text("Ruby/Rails");
-    }
-    else if (max === css) {
-      $("#result").show();
-      $("span.path").text("CSS/React");
-    }
-    else {
-      $("#result").show();
-      $("span.path").text("C#/.NET");
-    }
 
-  });
+
 });
